6cf7af79b401a3965260b3f0f120eaa614609171
unknown <francisyanga@foundationfootprint.com>
2015-07-29 22:25:47 +1200

    document builder change to simplier UI

---
+++ tortuga.MongoData/Entities/Model/DocumentSchema.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tortuga.MongoData.Entities.Base;
+
+namespace tortuga.MongoData.Entities.Model
+{
+    public class DocumentSchema : MongoEntity
+    {
+        public String Name { get; set; }
+        public List<Field> Fields { get; set; }
+    }
+
+    public class Field
+    {
+        public String Name { get; set; }
+        public DataType DataType { get; set; }
+        public List<Field> Fields { get; set; }
+    }
+
+    public enum DataType
+    {
+        String = 0,
+        Number = 1,
+        Document = 3
+    }
+}
--- tortuga/Controllers/DocumentController.cs
+++ tortuga/Controllers/DocumentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tortuga.MongoData.Entities.Model;
 
 namespace tortuga.Controllers
 {
@@ -14,6 +15,39 @@ namespace tortuga.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult DocumentEditor()
+        {
+            DocumentSchema document = new DocumentSchema();
+            document.Name = "Inventory";
+            var fieds = new List<Field>();
+            fieds.Add(new Field
+            {
+                DataType = DataType.String,
+                Name = "Item Name"
+            });
+            fieds.Add(new Field
+            {
+                DataType = DataType.String,
+                Name = "Item Description"
+            });
+
+
+            var subProducts = new List<Field>();
+            subProducts.Add(new Field { Name = "Name", DataType = DataType.String });
+            subProducts.Add(new Field { Name = "Description", DataType = DataType.String });
+            subProducts.Add(new Field { Name = "Quantity", DataType = DataType.Number });
+
+            fieds.Add(new Field
+            {
+                DataType = DataType.Document,
+                Name = "Sub Products",
+                Fields = subProducts
+            });
+            document.Fields = fieds;
+
+            return View(document);
         }
 
     }
